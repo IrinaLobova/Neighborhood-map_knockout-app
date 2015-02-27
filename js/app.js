3044bef9f7dc6b048c01ffb0a3524099e5d8545c
Irina Lobova <lobovais@gmail.com>
2015-02-26 22:47:16 -0500

    knockout code polish

--- js/app.js
+++ js/app.js
@@ -1,32 +1,37 @@
-// Project 5, neighborhood map for Udacidy Frontend Nanodegree
-// Author: Irina Lobova
+/* Project 5, neighborhood map for Udacidy Frontend Nanodegree
+   Author: Irina Lobova */
 
-var initialLocation;
-var browserSupportFlag =  new Boolean();
-var q = {lat: 42.36690, lng: -71.10610, food: 'thai'}
-var geocoder;
-var map;
-var AllLocations = ko.observableArray([]);
-var markers = [];
+var q = {lat: 42.36690, lng: -71.10610, food: 'thai'}; /* initial lat, lng and food type */
+document.addEventListener('DOMContentLoaded', ko.applyBindings(new ViewModel()));
 
-document.addEventListener('DOMContentLoaded', main());
+function ViewModel() {
+  var self = this;
+  var markers = [];
+  self.allLocations = ko.observableArray([]);
+  self.map = ko.observable(initializeMap());
+  self.geocoder = ko.observable(initializeGeo(self.map()));
+  self.clickHandler = function(data) { moveMap(data, self.map(), markers) };
+  self.submitHandler = function() { updateLocation(self.map(), self.geocoder(), self.allLocations, markers) };
 
-function main () {
-  initialize();// loads google map only after AllLocations and dataArray are loaded
-  ko.applyBindings(new ViewModel());
-};
+  self.submitHandler();
+}
 
-function initialize() {
+function initializeMap() {
   geocoder = new google.maps.Geocoder();
-  var cambridge = new google.maps.LatLng(q.lat, q.lng);
   var mapOptions = {
       center: {lat: q.lat, lng: q.lng},
       zoom: 12,
       mapTypeId: google.maps.MapTypeId.ROADMAP
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  return new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+}
 
-  // Geolocation
+function initializeGeo(map) {
+  var cambridge = new google.maps.LatLng(q.lat, q.lng);
+  var browserSupportFlag;
+  var initialLocation;
+  
+  /* Geolocation */
   if(navigator.geolocation) {
     browserSupportFlag = true;
     navigator.geolocation.getCurrentPosition(function(position) {
@@ -39,11 +44,13 @@ function initialize() {
       handleNoGeolocation(browserSupportFlag);
     });
   }
-  // Browser doesn't support Geolocation
+  /* Browser doesn't support Geolocation */
   else {
     browserSupportFlag = false;
     handleNoGeolocation(browserSupportFlag);
   }
+
+  return geocoder;
 
   function handleNoGeolocation(errorFlag) {
     if (errorFlag == true) {
@@ -55,36 +62,10 @@ function initialize() {
   }
 } 
 
-// Takes address (value from the input box), converts it to new lat and lng and updates the map.
-// Then calls ViewModel to update markers
-function codeAddress() {
-  var address = document.getElementById('address').value;
-  geocoder.geocode( { 'address': address }, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      var newLatlng  = new google.maps.LatLng(results[0].geometry.location);
-      map.setCenter(results[0].geometry.location);
-      q.lat = results[0].geometry.location.k;
-      q.lng = results[0].geometry.location.D;
-      q.food = document.getElementById('food').value;
-      clearMarkers(); //clear previous markers
-      ViewModel();
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
-  });
-}
- 
-function clearMarkers() {
-  for (var i = 0; i < markers.length; i++) {
-    markers[i].setMap(null);
-  }
-  markers = [];
-} 
-
-function ViewModel() {
-  AllLocations.removeAll(); //removes previous items from the list
+function fetchFoursquare(map, allLocations, markers) {
+  allLocations([]); /* removes previous items from the list */
   
-  //Foursquare API request
+  /* Foursquare API request */
   var foursquareUrl = 'https://api.foursquare.com/v2/venues/search' +
   '?client_id=NEVWWB4QNWYIBNRSMMAMPSED3BZTNS3CSTSCHD1LLRR01A2U' +
   '&client_secret=DZZOB2RCPHICI3YUWZDVW2BPCY3B5AXJMOLPEDY4WZPAPPAL' +
@@ -93,22 +74,23 @@ function ViewModel() {
   '&v=20140806' +
   '&m=foursquare';
   
-  var dataArray = []; //Array of venues' names and longitudes and latitudes 
+  var dataArray = []; /* Array of venues' names, longitudes and latitudes */ 
 
   $.getJSON(foursquareUrl, function(data) {
-    // Once we get the JSON data, put data into an observable array.
+    /* Once we get the JSON data, put data into an observable array */
     for (var index in data.response.venues) { 
       var item = data.response.venues[index];
-      AllLocations.push(item);
-      dataArray.push({lat: item.location.lat, lng: item.location.lng, name: item.name});//put data to LongLatArray 
+      allLocations.push(item);
+      /* put data to dataArray */
+      dataArray.push({lat: item.location.lat, lng: item.location.lng, name: item.name});
     }
-    setMarkers(dataArray);
-  }); 
+    setMarkers(dataArray, map, markers);
+  });
 } 
 
-//Takes lng and lat from dataArray and applies them to set markers on the map
-function setMarkers (dataArray) {
-  for (var index in dataArray) {
+/* Take lng and lat from dataArray and apply them to set markers on the map */
+function setMarkers(dataArray, map, markers) {
+  for (var index in dataArray) {  
     var element = dataArray[index];
     var newLatlng = new google.maps.LatLng(element.lat, element.lng);
     var marker = new google.maps.Marker({
@@ -119,7 +101,7 @@ function setMarkers (dataArray) {
     });
     markers.push(marker);
     
-    //create infowindows for each of the markers
+    /* create infowindows for each of the markers */
     var infowindow = new google.maps.InfoWindow();
     google.maps.event.addListener(marker, 'click', function() {
       infowindow.open(map, this);
@@ -128,19 +110,51 @@ function setMarkers (dataArray) {
   }
 }
 
-//When the link in the list is clicked, take lat and lng from the passed data and set the map zoom 
-//according to these parametres 
-function clickHandler(data) {
+/* Submit handler */
+
+/* Take address (value from the input box), 
+   convert it to new lat and lng and update the map.
+   Then call fetchFoursquare to update markers */
+function updateLocation(map, geocoder, allLocations, markers) {
+  var address = document.getElementById('address').value;
+  if (address === '') fetchFoursquare(map, allLocations, markers);
+  else geocoder.geocode( { 'address': address }, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      var newLatlng  = new google.maps.LatLng(results[0].geometry.location);
+      map.setCenter(results[0].geometry.location);
+      q.lat = results[0].geometry.location.k;
+      q.lng = results[0].geometry.location.D;
+      q.food = document.getElementById('food').value;
+      clearMarkers(markers); /* clear previous markers */     
+      fetchFoursquare(map, allLocations, markers);
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+ 
+function clearMarkers(markers) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
+} 
+
+/* clickHandler */
+
+/* When the link in the list is clicked, take lat and lng from the passed data and set the map zoom 
+   according to these parametres */ 
+function moveMap(data, map, markers) {
   map.setCenter(new google.maps.LatLng(data.location.lat, data.location.lng));
   map.setZoom(14);
   for (var i = 0; i < markers.length; i++) {    
     if (data.name === markers[i].content) {      
       toggleBounce(markers[i]); // apply animation
     }
-  } 
+  }
 }
 
-//Create animation for the marker when its corresponding li is clicked
+/* Create animation for the marker when its corresponding li is clicked */
 function toggleBounce(currentIcon) {
   if (currentIcon.getAnimation() != null) {
     currentIcon.setAnimation(null);
